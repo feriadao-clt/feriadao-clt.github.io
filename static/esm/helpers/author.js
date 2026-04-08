1334ff6ba55df7ba00a9047b98c37ad6e423363a
inavleb <del.cidrak@gmail.com>
2026-04-08 00:13:12 -0300

    updates

--- static/esm/helpers/author.js
+++ static/esm/helpers/author.js
@@ -3,7 +3,7 @@ $(document.body).on("click", "[data-author]", function() {
   const dialogPrint = (title, content) => $.dialog({ title, content });
   
   if (value === "ABOUT") Snackbar.show({ text: "Powred by Delvani." });
-  if (value === "LICENSE") dialogPrint("LICENSE", "url:/static/page/faq/LICENSE.html");
+  if (value === "LICENSE") dialogPrint("LICENSE", "url:/static/page/faq/LICENSE.txt");
   return false;
 });
 
