931c06f2c0f4c26a05e433b2672092cd667611e8
inavleb <del.cidrak@gmail.com>
2026-04-11 06:41:12 -0300

    updates

--- static/esm/helpers/license.js
+++
@@ -1,7 +0,0 @@
-const res = await fetch("/static/page/faq/license.md");
-const html = await res.text();
-const modal = $(html).appendTo(document.body);
-
-modal.on("click", '[data-btn-download="license"]', evt => download("/LICENSE", "LICENSE.txt"));
-
-export default null;
